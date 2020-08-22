b57e755cbf4d6429309b8096d27fa17195f539b9
Marisol Dourado <mbernardo@serviceup.com.br>
2020-08-22 12:04:25 -0300

    Login Constante (Aplicativo que realiza o login com usurio e senha definidos em constantes)

---
+++ app/src/main/java/br/edu/dmos5/loginlocal/BemVindoActivity.java
@@ -0,0 +1,14 @@
+package br.edu.dmos5.loginlocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BemVindoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bem_vindo);
+    }
+}
